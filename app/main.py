86051d0fbcd937db7dcf41cebb192e05fcc2e86e
Maxim <kuzalkovmaxim@gmail.com>
2024-12-05 18:27:46 +0100

    Added docker and pipevn config files.

--- app/main.py
+++ app/main.py
@@ -1,17 +1,54 @@
-import asyncio
-from sqlalchemy import create_engine, text
-from sqlalchemy.ext.asyncio import create_async_engine
+import logging
+from contextlib import asynccontextmanager
+from time import perf_counter
 
-from config import settings
-from sqlalchemy import create_engine
+import sqlalchemy as sa
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+from starlette.responses import RedirectResponse, Response
 
-sync_engine = create_engine(
-    url=settings.DATABASE_URL_psycopg,
-    echo=True,
-    pool_size=5,
-    max_overflow=10
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-with sync_engine.connect() as conn:
-    res = conn.execute(text("SELECT 1,2,3 union 4,5,6"))
-    print(f"{res.first()=}")
+
+@app.get("/")
+async def root():
+    return RedirectResponse("/docs")
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = perf_counter()
+    response = await call_next(request)
+    process_time = perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(round(process_time, 5))
+    return response
+
+
+@app.exception_handler(Exception)
+async def debug_exception_handler(request: Request, exc: Exception):
+    import traceback
+
+    logging.exception(exc)
+
+    return Response(
+        content="".join(
+            traceback.format_exception(type(exc), value=exc, tb=exc.__traceback__)
+        ),
+        status_code=500,
+    )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    logging.basicConfig(level=logging.INFO)
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ to_do.md
@@ -0,0 +1,8 @@
+Things to install:
+    - Docker
+    - Docker_Compose
+    - Pycharm
+    - Gitkraken
+Resources:
+    - https://gitlab.com/suka.work/devops-course/-/blob/draft2/lessons/lesson-3-docker-basics.md?ref_type=heads
+    - codeium
